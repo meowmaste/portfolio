73541404f2d30ede79a60ca260eaa6831b490ecb
Joslyn Rosbrook <joslynrosbrook@Joslyns-MacBook-Air.local>
2014-01-15 20:13:22 -0800

    adding fixtures

--- test/features/deleting_a_post_test.rb
+++ test/features/deleting_a_post_test.rb
@@ -3,14 +3,15 @@ require "test_helper"
 feature "Deleting A Post" do
   scenario "post is deleted when button is pressed" do
     #Given an existing post
-    Post.create(title: "Becoming a Code Fellow", body: "Means striving for excellence.")
+
     visit posts_path
+    title = posts(:http).title
 
     #When delete button is clicked
-    click_on "Destroy"
+    first(:link, "Destroy").click
 
     #Then the post is deleted
-    page.wont_have_content "Becoming a Code Fellow"
+    page.wont_have_content title
 
   end
 end
