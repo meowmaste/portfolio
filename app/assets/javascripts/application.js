1b80a4be0b3a36adf7185f7a22d987bf2a025500
Joslyn Rosbrook <joslynrosbrook@Joslyns-MacBook-Air.local>
2014-01-19 13:26:55 -0800

    styling using foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree ../../../vendor/assets/javascripts/zurb/
 //= require turbolinks
 //= require_tree .
 
--- test/features/creating_a_post_test.rb
+++ test/features/creating_a_post_test.rb
@@ -4,14 +4,14 @@ feature "Creating A Post" do
   scenario "submit form data to create a new post" do
     #Given a completed new post form
     visit new_post_path
-    fill_in "Title", with: posts(:cr).title
-    fill_in "Body", with: posts(:cr).body
+    fill_in "Title", with: "Luna"
+    fill_in "Body", with: "...is a cool cat!"
 
     #When I submit the form
     click_on "Create Post"
 
     #Then a new post should be created and displayed
     page.text.must_include "Post was successfully created"
-    page.text.must_include posts(:cr).title
+    page.text.must_include "Luna"
   end
 end
