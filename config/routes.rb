17f7d2533261115b6d9ed93c1a78be0034ec6d74
Joslyn <jrosbrook@gmail.com>
2014-01-26 19:59:18 -0800

    add contact me page, link to on home nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Portfolio::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get '/contact' => 'static_pages#contact_me'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
