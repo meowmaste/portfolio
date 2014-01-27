8c95e13024de3ee3bcdb96e4a1896fbf8a794bf4
Joslyn <jrosbrook@gmail.com>
2014-01-26 19:52:46 -0800

    changed home controller to a static page controller for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Portfolio::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
