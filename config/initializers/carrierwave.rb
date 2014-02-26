d496dd352af15de6c739d91fae21a41e4e15dee4
Joslyn <jrosbrook@gmail.com>
2014-02-25 18:42:02 -0800

    Securing AWS creds

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAIN42BSODVCZKP57A',                        # required
-    :aws_secret_access_key  => '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',                        # required
+    :aws_access_key_id      => ENV['AWS_ACCESS'],                        # required
+    :aws_secret_access_key  => ENV['AWS_SECRET'],                        # required
     :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'portfolio.joslynrosbrook'                     # required
